a6bd86b5e4271196ae242ff123a99069f01b1ab2
sourabhamancha <sourabh.amancha@gmail.com>
2020-07-05 00:35:47 +0530

    second commit

---
+++ frontend/build/precache-manifest.814b0863da07c1d986cd3631428a0b9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c378c7baa6b0270a334bf357c31d3d24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff47e5412195f1ac9063",
+    "url": "/static/css/main.10ad542b.chunk.css"
+  },
+  {
+    "revision": "f65cff2ca0dae9390241",
+    "url": "/static/js/2.ce87e72f.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.ce87e72f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff47e5412195f1ac9063",
+    "url": "/static/js/main.f5cb13cc.chunk.js"
+  },
+  {
+    "revision": "707eb649b137d38171a0",
+    "url": "/static/js/runtime-main.5f668b5a.js"
+  },
+  {
+    "revision": "4b1bb228bf826befdb5e9d530b8738fa",
+    "url": "/static/media/no-image.4b1bb228.png"
+  }
+]);
